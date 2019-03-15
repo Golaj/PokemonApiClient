391aba7fbbd84fa0c457533fe1a3b6dc3ddfe252
Golaj <ch.rehn90@gmail.com>
2019-03-15 14:44:25 +0100

    Added method for getting all pokemons and pokemons of specific type

--- src/java/Client/PokeDex.java
+++ src/java/Client/PokeDex.java
@@ -20,8 +20,10 @@ public class PokeDex {
     public Pokemon getOnePokemon(String id) {
         return target.path(id).request().get(Pokemon.class);
     }
-    public List<Pokis> getAllPokisOnFirstPage() {
+    public List<Pokis> getAllPokemons() {
         PokemonList allPs = target.request().get(PokemonList.class);
+        String pageURL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=" + allPs.getCount();
+        allPs = client.target(pageURL).request().get(PokemonList.class);
         return allPs.getResults();
     }
     
@@ -29,6 +31,27 @@ public class PokeDex {
         String pageURL = "https://pokeapi.co/api/v2/pokemon?offset=" + number + "0&limit=20";
         PokemonList pokemons = client.target(pageURL).request().get(PokemonList.class);
         return pokemons.getResults();
+    }
+    
+        public List<Pokemon> getAllPokemonOfSpecificType(String type) {
+        List<Pokis> allPokis = getAllPokemons();
+        List<Pokemon> allPokemon = new ArrayList<>();
+        Pokemon p;
+        for (Pokis poki : allPokis) {
+            String type1;
+            String type2 = "";
+            p = client.target(poki.getUrl()).request().get(Pokemon.class);
+            if (p.getTypes().size() > 1) {
+                type1 = p.getTypes().get(0).getType().getName();
+                type2 = p.getTypes().get(1).getType().getName();
+            } else {
+                type1 = p.getTypes().get(0).getType().getName();
+            }
+            if (type1.equalsIgnoreCase(type) || type2.equalsIgnoreCase(type)) {
+                allPokemon.add(p);
+            }
+        }
+        return allPokemon;
     }
 
 }
